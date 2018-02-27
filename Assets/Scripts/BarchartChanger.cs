f1548cd15072a0ffbf46375281f3fccf66d66dc7
alex-ong <the.onga@gmail.com>
2018-02-28 00:04:26 +1100

    Added BarchartChanger and BarchartPair

---
+++ Assets/Scripts/BarchartChanger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BarchartChanger : MonoBehaviour {
+    public RectTransform bar;
+    public Text t;
+    public float maxHeight = 100f;
+    public float currentValue = 0.0f;
+
+    public void SetNumber(int number, float perc)
+    {
+        Rect r = bar.rect;
+        r.height = perc * maxHeight;
+        t.text = number.ToString();
+        currentValue = number;
+    }
+
+    public void SetNumber(float number, float perc)
+    {
+        Rect r = bar.rect;
+        r.height = perc * maxHeight;
+        t.text = number.ToString("0.00");
+        currentValue = number;
+    }
+
+}
